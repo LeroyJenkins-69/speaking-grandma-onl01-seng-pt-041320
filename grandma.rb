d4a97fe557c851d3c01074bcbf8b6ba6f43764fd
Kordell Parrish <parrishkordell@gmail.com>
2020-04-17 01:56:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 def speak_to_grandma(phrase)
   if "#{phrase}" == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif "#{phrase}" == "FUCK"
     puts "NO, NOT SINCE 1938!"
   else
